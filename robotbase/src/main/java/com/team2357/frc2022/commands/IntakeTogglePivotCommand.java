3bffbf6ce4402ecf92bbb96128da0ce0932e05da
NolanCampbell2357 <nolanc1223@gmail.com>
2022-01-22 13:07:29 -0600

    add intake toggle command

--- robotbase/src/main/java/com/team2357/frc2022/commands/IntakeTogglePivotCommand.java
+++ robotbase/src/main/java/com/team2357/frc2022/commands/IntakeTogglePivotCommand.java
@@ -12,21 +12,23 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
  * @category Intake
  */
 public class IntakeTogglePivotCommand extends CommandLoggerBase {
+    private IntakeSubsystem m_intakeSub;
 
     /**
      * @param intakeSubsystem The {@link IntakeSubsystem}.
      */
-    public IntakeTogglePivotCommand() {
-
+    public IntakeTogglePivotCommand(IntakeSubsystem intakeSub) {
+        m_intakeSub = intakeSub;
+        addRequirements(m_intakeSub);
     }
-    
-    @Override 
+
+    @Override
     public void initialize() {
-        
+        m_intakeSub.setPivot(m_intakeSub.getPivot() == Value.kReverse ?Value.kForward: Value.kReverse);
     }
 
     @Override
     public boolean isFinished() {
         return true;
-    } 
+    }
 }
