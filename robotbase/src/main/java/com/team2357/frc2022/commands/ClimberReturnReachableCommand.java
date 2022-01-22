2e1bda39ebbce4f347a7660c35bb3e266d93cc03
NolanCampbell2357 <nolanc1223@gmail.com>
2022-01-22 15:28:39 -0600

    Start adding climber member methods

--- robotbase/src/main/java/com/team2357/frc2022/commands/ClimberReturnReachableCommand.java
+++ robotbase/src/main/java/com/team2357/frc2022/commands/ClimberReturnReachableCommand.java
@@ -1,21 +1,24 @@
 package com.team2357.frc2022.commands;
 
+import com.team2357.frc2022.subsystems.ClimberSubsystem;
 import com.team2357.lib.commands.CommandLoggerBase;
 
 public class ClimberReturnReachableCommand extends CommandLoggerBase {
+    private ClimberSubsystem m_climberSub;
 
-    public ClimberReturnReachableCommand() {
 
+    public ClimberReturnReachableCommand(ClimberSubsystem climberSub) {
+        m_climberSub = climberSub;
     }
 
     @Override
     public void initialize() {
-
+        m_climberSub.returnReachable();
     }
 
     @Override
     public void execute() {
-
+        
     }
 
     @Override
--- robotbase/src/main/java/com/team2357/frc2022/commands/ClimberSetPivotCommand.java
+++ robotbase/src/main/java/com/team2357/frc2022/commands/ClimberSetPivotCommand.java
@@ -2,6 +2,7 @@ package com.team2357.frc2022.commands;
 
 import com.team2357.lib.commands.CommandLoggerBase;
 
+//TODO: Make Tyson write this one
 public class ClimberSetPivotCommand extends CommandLoggerBase {
 
 }
