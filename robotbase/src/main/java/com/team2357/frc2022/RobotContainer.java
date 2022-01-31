61d681c7d21c0614cfd78a75d68ef79920f400aa
NolanCampbell2357 <nolanc1223@gmail.com>
2022-01-30 19:24:37 -0600

    Fix intake controls and update wpilib

--- robotbase/src/main/java/com/team2357/frc2022/RobotContainer.java
+++ robotbase/src/main/java/com/team2357/frc2022/RobotContainer.java
@@ -29,7 +29,7 @@ public class RobotContainer {
   private FalconTrajectoryDriveSubsystem m_driveSub;
   private IntakeSubsystem m_intakeSub;
 
-  private final InvertDriveControls m_driverControls;
+  private final IntakeDriveControls m_driverControls;
   private final GunnerControls m_gunnerControls;
 
   /**
