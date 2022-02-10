569e0b3487fea6691d32afa5c4dc08b999b9246f
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2022-02-10 15:41:30 -0800

    h

---
+++ robotbase/src/main/java/com/team2357/frc2022/shuffleboard/ShuffleboardWidget.java
@@ -0,0 +1,18 @@
+package com.team2357.frc2022.shuffleboard;
+
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+
+public abstract class ShuffleboardWidget {
+    private final String m_tabTitle;
+
+    protected ShuffleboardWidget(String tabTitle) {
+        m_tabTitle = tabTitle;
+    }
+
+    public void show() {
+        Shuffleboard.selectTab(m_tabTitle);
+    }    
+
+    public void periodic() {
+    }
+}
