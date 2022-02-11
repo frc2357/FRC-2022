35110e1ebf07af9304e6aaeff8f8f8e8eedeab5b
Noah2357 <noah.schmidt498@raypec.org>
2022-02-10 17:12:29 -0800

    h

--- robotbase/src/main/java/com/team2357/frc2022/shuffleboard/FailsafeButtonWidget.java
+++ robotbase/src/main/java/com/team2357/frc2022/shuffleboard/FailsafeButtonWidget.java
@@ -1,31 +1,27 @@
 package com.team2357.frc2022.shuffleboard;
 
-// TODO: ShuffleBoard Implement
-/**
- * This class should have a option to toggle failsafe mode.
- * Should add a widget to a given shuffleboard tab that:
- * Creates a widget on given tab that has label "FAILSAFE" and is a toggleButton
- * Creates a toggleTrigger based on the created widget
- * The trigger should set the subsystems to failsafe mode when it is active, and
- * inactive when the trigger is inactive
- * 
- * Further information
- * 
- * The method setClosedLoopEnabled is on all our subsystems
- * If it is set to true, the subsystem will not be in closed loop mode
- * If it is set to false, the subsystem will be in open loop mode
- * 
- * If in failsafe mode, all subsystems should be in open loop mode
- * 
- * Closed loop vs. open loop
- * Closed loop: Susbystem is relying on sensors to track position and function
- * Open loop: Subsystem is only taking input from human controllers, and does
- * not have any positional data.
- * 
- * You should use the FailsafeCommand to set subsystems to their respective
- * closed loop state
- * 
- */
-public class FailsafeButtonWidget {
+import com.team2357.frc2022.commands.FailsafeCommand;
+import com.team2357.lib.subsystems.ClosedLoopSubsystem;
+import com.team2357.lib.triggers.ToggleTrigger;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+
+public class FailsafeButtonWidget extends ShuffleboardWidget {
+    private final NetworkTableEntry m_failsafeButton;
+    private ToggleTrigger failsafeTrigger;
+
+    public FailsafeButtonWidget(String tabTitle, ClosedLoopSubsystem[] subsystems) {
+        super(tabTitle);
+        m_failsafeButton = Shuffleboard.getTab(tabTitle)
+            .add("FAILSAFE", false)
+            .withWidget(BuiltInWidgets.kToggleButton)
+            .getEntry();
+        
+        failsafeTrigger = new ToggleTrigger(m_failsafeButton);
+        failsafeTrigger.whenActive(new FailsafeCommand(true, subsystems));
+        failsafeTrigger.whenInactive(new FailsafeCommand(false, subsystems));
+
+    }
 }
