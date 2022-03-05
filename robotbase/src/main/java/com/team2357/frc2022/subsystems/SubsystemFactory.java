6f58ac92f8b14020af5f2fb352330b7324840c40
NolanCampbell2357 <nolanc1223@gmail.com>
2022-03-05 14:37:39 -0600

    Fix formatting

--- robotbase/src/main/java/com/team2357/frc2022/subsystems/SubsystemFactory.java
+++ robotbase/src/main/java/com/team2357/frc2022/subsystems/SubsystemFactory.java
@@ -22,60 +22,59 @@ import edu.wpi.first.wpilibj.PneumaticsModuleType;
  * @category Subsystems
  */
 public class SubsystemFactory {
-        public SubsystemFactory() {
-        }
+    public SubsystemFactory() {
+    }
 
-        public FalconTrajectoryDriveSubsystem CreateFalconTrajectoryDriveSubsystem() {
+    public FalconTrajectoryDriveSubsystem CreateFalconTrajectoryDriveSubsystem() {
+            
+        WPI_TalonFX leftFalconMaster = Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_LEFT_1,
+            Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS);
 
-                WPI_TalonFX leftFalconMaster = Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_LEFT_1,
-                                Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS);
+        WPI_TalonFX[] leftFalconSlaves = new WPI_TalonFX[] {
+        Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_LEFT_2,
+            Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS),
+        Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_LEFT_3,
+            Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS) };
 
-                WPI_TalonFX[] leftFalconSlaves = new WPI_TalonFX[] {
-                                Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_LEFT_2,
-                                                Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS),
-                                Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_LEFT_3,
-                                                Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS) };
+        WPI_TalonFX rightFalconMaster = Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_RIGHT_1,
+            Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS);
 
-                WPI_TalonFX rightFalconMaster = Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_RIGHT_1,
-                                Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS);
+        WPI_TalonFX[] rightFalconSlaves = new WPI_TalonFX[] {
+            Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_RIGHT_2,
+                    Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS),
+            Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_RIGHT_3,
+                    Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS) };
 
-                WPI_TalonFX[] rightFalconSlaves = new WPI_TalonFX[] {
-                                Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_RIGHT_2,
-                                                Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS),
-                                Utility.createDriveTalonFX(Constants.CAN_ID.DRIVE_MOTOR_RIGHT_3,
-                                                Constants.DRIVE.DRIVE_MOTOR_RAMP_RATE_SECONDS) };
+        PigeonIMU gyro = new PigeonIMU(Constants.CAN_ID.GYRO_ID);
 
-                PigeonIMU gyro = new PigeonIMU(Constants.CAN_ID.GYRO_ID);
+        FalconTrajectoryDriveSubsystem subsystem = new FalconTrajectoryDriveSubsystem(leftFalconMaster,
+            leftFalconSlaves, rightFalconMaster, rightFalconSlaves, gyro,
+            Constants.DRIVE.ENCODER_DISTANCE_PER_PULSE_METERS,
+            Constants.DRIVE.LEFT_ENCODER_CHANNEL_A, Constants.DRIVE.LEFT_ENCODER_CHANNEL_B,
+            Constants.DRIVE.RIGHT_ENCODER_CHANNEL_A, Constants.DRIVE.RIGHT_ENCODER_CHANNEL_B);
 
-                FalconTrajectoryDriveSubsystem subsystem = new FalconTrajectoryDriveSubsystem(leftFalconMaster,
-                                leftFalconSlaves, rightFalconMaster, rightFalconSlaves, gyro,
-                                Constants.DRIVE.ENCODER_DISTANCE_PER_PULSE_METERS,
-                                Constants.DRIVE.LEFT_ENCODER_CHANNEL_A, Constants.DRIVE.LEFT_ENCODER_CHANNEL_B,
-                                Constants.DRIVE.RIGHT_ENCODER_CHANNEL_A, Constants.DRIVE.RIGHT_ENCODER_CHANNEL_B);
+        subsystem.configure(Constants.DRIVE.GET_FALCON_DRIVE_CONFIG());
+        return subsystem;
+    }
 
-                subsystem.configure(Constants.DRIVE.GET_FALCON_DRIVE_CONFIG());
-                return subsystem;
+    public IntakeSubsystem CreateIntakeSubsystem(SensorBooleanState intakeSensorState) {
+        DoubleSolenoid intakeDoubleSolenoid = new DoubleSolenoid(Constants.CAN_ID.PNEUMATICS_HUB_ID,
+            PneumaticsModuleType.REVPH, Constants.PH_ID.INTAKE_SOLENOID_FORWARD_CHANNEL,
+            Constants.PH_ID.INTAKE_SOLENOID_REVERSE_CHANNEL);
+        VictorSPX intakeVictor = new VictorSPX(Constants.CAN_ID.INTAKE_MOTOR_ID);
+        return new IntakeSubsystem(intakeVictor, intakeDoubleSolenoid, intakeSensorState);
+    }    
 
-        }
+    public FeederSubsystem CreateFeederSubsystem(SensorBooleanState feederSensorState) {
+        WPI_TalonSRX feederTalon = new WPI_TalonSRX((Constants.CAN_ID.FEEDER_MOTOR_ID));
+        return new FeederSubsystem(feederTalon, feederSensorState);
+    }   
 
-        public IntakeSubsystem CreateIntakeSubsystem(SensorBooleanState intakeSensorState) {
-                DoubleSolenoid intakeDoubleSolenoid = new DoubleSolenoid(Constants.CAN_ID.PNEUMATICS_HUB_ID,
-                                PneumaticsModuleType.REVPH, Constants.PH_ID.INTAKE_SOLENOID_FORWARD_CHANNEL,
-                                Constants.PH_ID.INTAKE_SOLENOID_REVERSE_CHANNEL);
-                VictorSPX intakeVictor = new VictorSPX(Constants.CAN_ID.INTAKE_MOTOR_ID);
-                return new IntakeSubsystem(intakeVictor, intakeDoubleSolenoid, intakeSensorState);
-        }
-
-        public FeederSubsystem CreateFeederSubsystem(SensorBooleanState feederSensorState) {
-                WPI_TalonSRX feederTalon = new WPI_TalonSRX((Constants.CAN_ID.FEEDER_MOTOR_ID));
-                return new FeederSubsystem(feederTalon, feederSensorState);
-        }
-
-        public TogglableLimelightSubsystem CreateVisionSubsystem() {
-                TogglableLimelightSubsystem subsystem = new TogglableLimelightSubsystem(false);
-                subsystem.setPipeline(PipelineIndex.HUMAN_VIEW);
-                subsystem.setStream(false);
-                subsystem.setConfiguration(Constants.LIMELIGHT.GET_LIMELIGHT_SUBSYSTEM_CONFIG());
-                return subsystem;
-        }
+    public TogglableLimelightSubsystem CreateVisionSubsystem() {
+        TogglableLimelightSubsystem subsystem = new TogglableLimelightSubsystem(false);
+        subsystem.setPipeline(PipelineIndex.HUMAN_VIEW);
+        subsystem.setStream(false);
+        subsystem.setConfiguration(Constants.LIMELIGHT.GET_LIMELIGHT_SUBSYSTEM_CONFIG());
+        return subsystem;
+    }
 }
