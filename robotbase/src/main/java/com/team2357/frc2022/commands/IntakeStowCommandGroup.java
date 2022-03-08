2cec7efc21f26c35a252e273fb4d8cdc4fc0045a
NolanCampbell2357 <nolanc1223@gmail.com>
2022-03-08 17:32:26 -0600

    Cleanup

--- robotbase/src/main/java/com/team2357/frc2022/commands/IntakeStowCommandGroup.java
+++ robotbase/src/main/java/com/team2357/frc2022/commands/IntakeStowCommandGroup.java
@@ -6,14 +6,6 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import com.team2357.frc2022.Constants;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-<<<<<<< HEAD:robotbase/src/main/java/com/team2357/frc2022/commands/IntakeDeactivateCommandGroup.java
-public class IntakeDeactivateCommandGroup extends SequentialCommandGroup {
-    private IntakeSubsystem m_intakeSub;
-
-    public IntakeDeactivateCommandGroup(IntakeSubsystem intakeSub) {
-        System.out.println("Deactivating Intake");
-        m_intakeSub = intakeSub;
-=======
 /**
  * @category Intake
  */
@@ -21,7 +13,6 @@ public class IntakeStowCommandGroup extends SequentialCommandGroup {
     private IntakeSubsystem m_intakeSub;
 
     public IntakeStowCommandGroup(IntakeSubsystem intakeSub) {
->>>>>>> d8d4a5f890fa2c47c5aa9a16611af003d7f3f739:robotbase/src/main/java/com/team2357/frc2022/commands/IntakeStowCommandGroup.java
         addCommands(new IntakeRollerStop(m_intakeSub), new WaitCommand(Constants.INTAKE.ROLLER_STOP_SECONDS),
                 new IntakeSetPivotCommand(m_intakeSub, Value.kReverse));
         addRequirements(m_intakeSub);
@@ -38,5 +29,4 @@ public class IntakeStowCommandGroup extends SequentialCommandGroup {
         super.execute();
         System.out.println("Running deactivate intake");
     }
-
 }
