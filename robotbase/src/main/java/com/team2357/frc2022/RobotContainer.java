cf3009a1a2d67fc0806b9cab871f37962dc3e4f4
Tyson453 <95186148+Tyson453@users.noreply.github.com>
2022-03-08 17:30:33 -0600

    Change sensors to read from DIO instead of Arduino (#29)
    * Change sensors to read from DIO instead of Arduino
    
    * Got rid of turret sensor
    
    * Created DIO_IDS class in constants

--- robotbase/src/main/java/com/team2357/frc2022/RobotContainer.java
+++ robotbase/src/main/java/com/team2357/frc2022/RobotContainer.java
@@ -4,7 +4,6 @@
 
 package com.team2357.frc2022;
 
-import com.team2357.frc2022.arduino.RobotArduino;
 import com.team2357.frc2022.controls.GunnerControls;
 import com.team2357.frc2022.controls.IntakeDriveControls;
 import com.team2357.frc2022.sensors.SensorBooleanState;
@@ -19,6 +18,7 @@ import com.team2357.lib.subsystems.TogglableLimelightSubsystem;
 import com.team2357.lib.subsystems.drive.FalconTrajectoryDriveSubsystem;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -44,19 +44,23 @@ public class RobotContainer {
   private final IntakeDriveControls m_driverControls;
   private final GunnerControls m_gunnerControls;
 
-  private final RobotArduino m_arduinoSensor;
+  private final DigitalInput m_intakeSensor;
+  private final DigitalInput m_feederSensor;
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
   public RobotContainer() {
-    m_arduinoSensor = new RobotArduino(Constants.ARDUINO.ARDUINO_SENSOR_DEVICE_NAME);
+    m_intakeSensor = new DigitalInput(Constants.DIO_IDS.INTAKE_SENSOR_DIO_PORT);
+    m_feederSensor = new DigitalInput(Constants.DIO_IDS.FEEDER_SENSOR_DIO_PORT);
+
     SensorBooleanState intakeIRSensor = () -> {
-      return m_arduinoSensor.getIntakeValue();
+      return m_intakeSensor.get();
     };
     SensorBooleanState feederIRSensor = () -> {
-      return m_arduinoSensor.getFeederValue();
+      return m_feederSensor.get();
     };
+
 
     // Create subsystems
     SubsystemFactory subsystemFactory = new SubsystemFactory();
