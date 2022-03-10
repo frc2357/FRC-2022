b1eb6fc191eea4569c7fb1e764ca19a202924775
NolanCampbell2357 <nolanc1223@gmail.com>
2022-03-10 17:37:43 -0600

    Simplify climber

--- robotbase/src/main/java/com/team2357/frc2022/subsystems/ClimberSubsystem.java
+++ robotbase/src/main/java/com/team2357/frc2022/subsystems/ClimberSubsystem.java
@@ -2,47 +2,11 @@ package com.team2357.frc2022.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
-import com.team2357.frc2022.Constants;
-import com.team2357.frc2022.util.Utility;
 import com.team2357.lib.subsystems.ClosedLoopSubsystem;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
 
-/**
- * Subsystem to control climbing
- * 
- * Designed to rely on two validation checks to increase the progress of the
- * climber's state
- * The state of the climber helps regulate if a certain command along can be
- * ran. The states should only be changed linearly.
- * The validate method should only be used one command per climb
- * sequence
- * the method handleState increments the state, and handles reseting validation
- * set during each sequence.
- * 
- * The climb process for a successful Traversal climb:
- * 
- * Initial State: GROUND
- * Controller Input: Run ClimberExtendMidCommandGroup
- * Run handleState at end of sequence
- * New State: EXTEND_MID_RUNG
- * Controller Input: Run ClimberClimbMidCommandGroup
- * Run handleState at end of sequence
- * New State: ON_MID_RUNG
- * Controller Input: Run ClimberExtendHighCommandGroup
- * Run handleState at end of sequence
- * New State: EXTEND_HIGH_RUNG
- * If ClimberExtendHighCommandGroup successful: run ClimberClimbHighCommandGroup
- * Run handleState at end of sequence
- * New State: ON_HIGH_RUNG
- * Controller Input: Run ClimberExtendTraversalCommandGroup
- * Run handleState at end of sequence
- * New State: EXTEND_TRAVERSAL_RUNG
- * If ClimberExtendTraversalCommandGroup successful: run
- * ClimberClimbTraversalCommandGroup
- * Run handleState at end of sequence
- * New State: ON_TRAVERSAL_RUNG
- * 
+/* * 
  * 
  * @category climber
  */
@@ -52,41 +16,10 @@ public class ClimberSubsystem extends ClosedLoopSubsystem {
     public static class Configuration {
         public IdleMode m_climberMotorIdleMode = IdleMode.kBrake;
 
-        public int m_climberMissedToleranceRotations = 0;
-
         public int m_climberMotorStallLimitAmps = 0;
         public int m_climberMotorFreeLimitAmps = 0;
 
-        public double m_climberMotorP = 0;
-        public double m_climberMotorI = 0;
-        public double m_climberMotorD = 0;
-        public double m_climberMotorIZone = 0;
-        public double m_climberMotorFF = 0;
-        public double m_climberMotorMaxOutput = 0;
-        public double m_climberMotorMinOutput = 0;
-        public double m_climberMotorMaxRPM = 0;
-        public double m_climberMotorMaxVel = 0;
-        public double m_climberMotorMinVel = 0;
-        public double m_climberMotorMaxAcc = 0;
-        public double m_climberMotorAllowedError = 0;
-
-        public double m_climberRotationsCounterClockwiseSoftLimit = 0.0;
-        public double m_climberRotationsClockwiseSoftLimit = 0.0;
-        public double m_climberGearRatio = 0.0;
         public boolean m_isRightSideInverted = false;
-
-    }
-
-    public enum State {
-        GROUND,
-        EXTEND_LOW_RUNG,
-        ON_LOW_RUNG,
-        EXTEND_MID_RUNG,
-        ON_MID_RUNG,
-        EXTEND_HIGH_RUNG,
-        ON_HIGH_RUNG,
-        EXTEND_TRAVERSAL_RUNG,
-        TRAVERSAL_RUNG
     }
 
     private Configuration m_config;
@@ -94,9 +27,6 @@ public class ClimberSubsystem extends ClosedLoopSubsystem {
     private CANSparkMax m_rightClimberMotor;
     private DoubleSolenoid m_climberSolenoid;
     private Solenoid m_hookSolenoid;
-    private boolean m_isLeftClimberMotorValid;
-    private boolean m_isRightClimberMotorValid;
-    private State m_currentState;
 
     ClimberSubsystem(CANSparkMax leftClimberMotor, CANSparkMax rightClimberMotor, DoubleSolenoid climberSolenoid,
             Solenoid hookSolenoid) {
@@ -106,9 +36,6 @@ public class ClimberSubsystem extends ClosedLoopSubsystem {
         m_hookSolenoid = hookSolenoid;
         m_climberSolenoid.set(DoubleSolenoid.Value.kOff);
         m_hookSolenoid.set(false);
-        m_isLeftClimberMotorValid = false;
-        m_isRightClimberMotorValid = false;
-        m_currentState = State.GROUND;
     }
 
     public void configure(Configuration config) {
@@ -125,16 +52,6 @@ public class ClimberSubsystem extends ClosedLoopSubsystem {
                 m_config.m_climberMotorFreeLimitAmps);
     }
 
-    // Method to extend climber arms
-    public void extendClimber(double speed) {
-        this.climberMotorSpeed(speed);
-    }
-
-    // Method to return climber arms
-    public void retractClimber(double speed) {
-        this.climberMotorSpeed(-1 * speed);
-    }
-
     // Method to set climber speed
     public void climberMotorSpeed(double speed) {
         m_leftClimberMotor.set(speed);
@@ -145,72 +62,6 @@ public class ClimberSubsystem extends ClosedLoopSubsystem {
     public void StopClimberMotors() {
         m_leftClimberMotor.set(0);
         m_rightClimberMotor.set(0);
-    }
-
-    public boolean areMotorsValid() {
-        return m_isLeftClimberMotorValid && m_isRightClimberMotorValid;
-    }
-
-    public boolean isGripped(double rotations, int amps) {
-        boolean validMotorsExtension = atTargetRotations(rotations);
-        boolean[] validMotorsAmps = validateClimberMotorsAmps(amps);
-
-        if (validMotorsExtension && validMotorsAmps[0]) {
-            m_isLeftClimberMotorValid = true;
-        }
-        if (validMotorsExtension && validMotorsAmps[1]) {
-            m_isRightClimberMotorValid = true;
-        }
-        return areMotorsValid();
-    }
-
-    public boolean[] validateClimberMotorsAmps(int amps) {
-        boolean[] isAtAmps = new boolean[2];
-        if (checkLeftClimberMotorAmps(amps)) {
-            m_leftClimberMotor.set(0);
-            isAtAmps[0] = true;
-        }
-        if (checkRightClimberMotorAmps(amps)) {
-            m_rightClimberMotor.set(0);
-            isAtAmps[1] = true;
-        }
-        return isAtAmps;
-    }
-
-    public boolean atTargetRotations(double rotations) {
-
-        if (Utility.isWithinTolerance(getLeftClimberRotations(), rotations,
-                Constants.CLIMBER.EXTENSION_TOLERANCE_METERS)) {
-            m_isLeftClimberMotorValid = true;
-        }
-        if (Utility.isWithinTolerance(getRightClimberRotations(), rotations,
-                Constants.CLIMBER.EXTENSION_TOLERANCE_METERS)) {
-            m_isLeftClimberMotorValid = true;
-        }
-        return areMotorsValid();
-    }
-
-    public void resetValidation() {
-        m_isLeftClimberMotorValid = false;
-        m_isRightClimberMotorValid = false;
-    }
-
-    public boolean checkLeftClimberMotorAmps(int amps) {
-        return m_leftClimberMotor.getOutputCurrent() >= amps;
-    }
-
-    public boolean checkRightClimberMotorAmps(int amps) {
-        return m_rightClimberMotor.getOutputCurrent() >= amps;
-    }
-
-    public boolean handleState(State newState) {
-        if (areMotorsValid() && (newState.ordinal() == (m_currentState.ordinal() + 1))) {
-            m_currentState = newState;
-            resetValidation();
-            return true;
-        } else {
-            return false;
-        }
     }
 
     public void resetEncoders() {
@@ -241,13 +92,4 @@ public class ClimberSubsystem extends ClosedLoopSubsystem {
     public boolean isHookOpen() {
         return m_hookSolenoid.get();
     }
-
-    public State getState() {
-        return m_currentState;
-    }
-
-    public void setState(State newState) {
-        m_currentState = newState;
-    }
-
 }
