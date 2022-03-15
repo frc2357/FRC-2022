e8029ca02e1ab3e7e0dba478163082207fa869c7
NolanCampbell2357 <nolanc1223@gmail.com>
2022-03-15 18:55:02 -0500

    Add shooter axis control

--- robotbase/src/main/java/com/team2357/frc2022/subsystems/ShooterSubsystem.java
+++ robotbase/src/main/java/com/team2357/frc2022/subsystems/ShooterSubsystem.java
@@ -143,6 +143,11 @@ public class ShooterSubsystem extends ClosedLoopSubsystem {
         m_topMotor.set(ControlMode.Velocity, nativeSpeed);
     }
 
+    public void setShooterMotorsPercent(double percentOutput) {
+        m_leftBottomMotor.set(percentOutput);
+        m_topMotor.set(percentOutput);
+    }
+
     public boolean hasTarget() {
         return m_currentTarget != null;
     }
